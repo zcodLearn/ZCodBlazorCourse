049cd68cd6b5ecdd91271a06573c6217b6f8cbbf
azimi.h <azimi.h@daneshrefah.ir>
2024-12-15 00:57:11 -0800

    antdesign

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ZCodBlazorCourse
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddAntDesign();
             await builder.Build().RunAsync();
         }
     }
