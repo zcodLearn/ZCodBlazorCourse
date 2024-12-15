77f713ea63308d9c3f077baca9b7ce0a42a3a5e7
azimi.h <azimi.h@daneshrefah.ir>
2024-12-15 00:43:31 -0800

    mud blazor

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace ZCodBlazorCourse
 {
@@ -12,7 +13,7 @@ namespace ZCodBlazorCourse
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
     }
